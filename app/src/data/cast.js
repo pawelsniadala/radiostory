713e7beee60594288cac8fea522a38bc99c2e15e
Paweł Śniadała <biner.home@gmail.com>
2022-06-12 19:39:00 +0200

    Cast data update

---
+++ app/src/data/cast.js
@@ -0,0 +1,32 @@
+import castThumbnail1 from "../assets/cast/castThumbnail1.jpg"
+import castThumbnail2 from "../assets/cast/castThumbnail2.jpg"
+import castThumbnail3 from "../assets/cast/castThumbnail3.jpg"
+import castThumbnail4 from "../assets/cast/castThumbnail4.jpg"
+
+
+export const cast = [
+    {
+        id: "04c8b670-3309-459c-a9a2-4be65f9a4d89",
+        name: "Piotr Głowacki",
+        role: "Waldemar",
+        img: castThumbnail1
+    },
+    {
+        id: "1f62fda1-4bc5-4317-9765-9c4a7cacb585",
+        name: "Grzegorz Gzyl",
+        role: "Kubek",
+        img: castThumbnail2
+    },
+    {
+        id: "388baf81-fb32-4f9d-8105-079c34d2c009",
+        name: "Bogdan Kalus",
+        role: "Słomiński",
+        img: castThumbnail3
+    },
+    {
+        id: "c43fa865-9cfa-4f70-a3f9-02b7c7a090ff",
+        name: "Dariusz Majchrzak",
+        role: "Melik",
+        img: castThumbnail4
+    }
+]
