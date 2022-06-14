61a83f1f5b8c9ea3b26d159c9b2d328310289127
Paweł Śniadała <biner.home@gmail.com>
2022-06-14 22:55:19 +0200

    Photo update

--- app/src/data/photo.js
+++ app/src/data/photo.js
@@ -2,21 +2,29 @@ import photoFilmOriginal1 from "../assets/photo/film/original/photoFilmOriginal1
 import photoFilmOriginal2 from "../assets/photo/film/original/photoFilmOriginal2.jpg";
 import photoFilmOriginal3 from "../assets/photo/film/original/photoFilmOriginal3.jpg";
 import photoFilmOriginal4 from "../assets/photo/film/original/photoFilmOriginal4.jpg";
+import photoFilmOriginal5 from "../assets/photo/film/original/photoFilmOriginal5.jpg";
+import photoFilmOriginal6 from "../assets/photo/film/original/photoFilmOriginal6.jpg";
 
 import photoFilmThumbnail1 from "../assets/photo/film/thumbnail/photoFilmThumbnail1.jpg"
 import photoFilmThumbnail2 from "../assets/photo/film/thumbnail/photoFilmThumbnail2.jpg"
 import photoFilmThumbnail3 from "../assets/photo/film/thumbnail/photoFilmThumbnail3.jpg"
 import photoFilmThumbnail4 from "../assets/photo/film/thumbnail/photoFilmThumbnail4.jpg"
+import photoFilmThumbnail5 from "../assets/photo/film/thumbnail/photoFilmThumbnail5.jpg"
+import photoFilmThumbnail6 from "../assets/photo/film/thumbnail/photoFilmThumbnail6.jpg"
 
 import photoPlanOriginal1 from "../assets/photo/plan/original/photoPlanOriginal1.jpg";
 import photoPlanOriginal2 from "../assets/photo/plan/original/photoPlanOriginal2.jpg";
 import photoPlanOriginal3 from "../assets/photo/plan/original/photoPlanOriginal3.jpg";
 import photoPlanOriginal4 from "../assets/photo/plan/original/photoPlanOriginal4.jpg";
+import photoPlanOriginal5 from "../assets/photo/plan/original/photoPlanOriginal5.jpg";
+import photoPlanOriginal6 from "../assets/photo/plan/original/photoPlanOriginal6.jpg";
 
 import photoPlanThumbnail1 from "../assets/photo/plan/thumbnail/photoPlanThumbnail1.jpg"
 import photoPlanThumbnail2 from "../assets/photo/plan/thumbnail/photoPlanThumbnail2.jpg"
 import photoPlanThumbnail3 from "../assets/photo/plan/thumbnail/photoPlanThumbnail3.jpg"
 import photoPlanThumbnail4 from "../assets/photo/plan/thumbnail/photoPlanThumbnail4.jpg"
+import photoPlanThumbnail5 from "../assets/photo/plan/thumbnail/photoPlanThumbnail5.jpg"
+import photoPlanThumbnail6 from "../assets/photo/plan/thumbnail/photoPlanThumbnail6.jpg"
 
 export const photo = {
     "title": "Zdjęcia",
@@ -74,33 +82,30 @@ export const photo = {
                 height: "1259"
             }
         },
-
-
-
         {
-            id: "d1420048-089c-4405-ae84-109d956f2cb9",
+            id: "bfb0b057-1680-46ef-9c79-756f4738d06e",
             // title: "",
             // description: '',
             thumbnail: {
-                src: photoFilmThumbnail3,
+                src: photoFilmThumbnail5,
             },
             original: {
-                src: photoFilmOriginal3,
-                width: "1346",
-                height: "926"
+                src: photoFilmOriginal5,
+                width: "1230",
+                height: "615"
             }
         },
         {
-            id: "8497254e-6263-430b-ba65-a71270adde71",
+            id: "a0ac88da-5fd3-4376-aaa9-9096764993a3",
             // title: "",
             // description: '',
             thumbnail: {
-                src: photoFilmThumbnail4,
+                src: photoFilmThumbnail6,
             },
             original: {
-                src: photoFilmOriginal4,
-                width: "1920",
-                height: "1259"
+                src: photoFilmOriginal6,
+                width: "700",
+                height: "700"
             }
         }
     ],
@@ -115,7 +120,7 @@ export const photo = {
             "original": {
                 "src": photoPlanOriginal1,
                 "width": "1920",
-                "height": "1280"
+                "height": "1272"
             }
         },
         {
@@ -128,7 +133,7 @@ export const photo = {
             "original": {
                 "src": photoPlanOriginal2,
                 "width": "1920",
-                "height": "1272"
+                "height": "2685"
             }
         },
         {
@@ -140,36 +145,6 @@ export const photo = {
             },
             "original": {
                 "src": photoPlanOriginal3,
-                "width": "1346",
-                "height": "926"
-            }
-        },
-        {
-            id: "bbf88515-8d39-4f0f-bba2-f5712f042901",
-            // title: "",
-            // description: '',
-            "thumbnail": {
-                "src": photoPlanThumbnail4,
-            },
-            "original": {
-                "src": photoPlanOriginal4,
-                "width": "1920",
-                "height": "1259"
-            }
-        },
-
-
-
-
-        {
-            id: "750f9ea7-2586-47b6-955d-7fe4b0cced7d",
-            // title: "",
-            // description: '',
-            "thumbnail": {
-                "src": photoPlanThumbnail2,
-            },
-            "original": {
-                "src": photoPlanOriginal2,
                 "width": "1920",
                 "height": "1272"
             }
@@ -184,7 +159,33 @@ export const photo = {
             "original": {
                 "src": photoPlanOriginal4,
                 "width": "1920",
-                "height": "1259"
+                "height": "1263"
+            }
+        },
+        {
+            id: "49efd86d-6198-43eb-acff-ec67f7b97c18",
+            // title: "",
+            // description: '',
+            "thumbnail": {
+                "src": photoPlanThumbnail5,
+            },
+            "original": {
+                "src": photoPlanOriginal5,
+                "width": "1039",
+                "height": "771"
+            }
+        },
+        {
+            id: "3479d5ae-1e1a-492c-bfc7-b30bc9a3759f",
+            // title: "",
+            // description: '',
+            "thumbnail": {
+                "src": photoPlanThumbnail6,
+            },
+            "original": {
+                "src": photoPlanOriginal6,
+                "width": "780",
+                "height": "410"
             }
         }
     ]
