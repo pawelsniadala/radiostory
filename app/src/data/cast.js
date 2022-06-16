3b0ec9e0289d6fb4a4ad67b91363afcc2c7f96a0
Paweł Śniadała <biner.home@gmail.com>
2022-06-16 02:50:08 +0200

    Photo update

--- app/src/data/cast.js
+++ app/src/data/cast.js
@@ -1,32 +1,112 @@
-import castThumbnail1 from "../assets/cast/castThumbnail1.jpg"
-import castThumbnail2 from "../assets/cast/castThumbnail2.jpg"
-import castThumbnail3 from "../assets/cast/castThumbnail3.jpg"
-import castThumbnail4 from "../assets/cast/castThumbnail4.jpg"
+import castMainThumbnail1 from "../assets/cast/main/castMainThumbnail1.jpg"
+// import castMainThumbnail2 from "../assets/cast/main/castMainThumbnail2.jpg"
+// import castMainThumbnail3 from "../assets/cast/main/castMainThumbnail3.jpg"
+// import castMainThumbnail4 from "../assets/cast/main/castMainThumbnail4.jpg"
 
+import castOtherThumbnail1 from "../assets/cast/other/castOtherThumbnail1.jpg"
+// import castOtherThumbnail2 from "../assets/cast/other/castOtherThumbnail2.jpg"
+// import castOtherThumbnail3 from "../assets/cast/other/castOtherThumbnail3.jpg"
+// import castOtherThumbnail4 from "../assets/cast/other/castOtherThumbnail4.jpg"
 
-export const cast = [
-    {
-        id: "04c8b670-3309-459c-a9a2-4be65f9a4d89",
-        name: "Piotr Głowacki",
-        role: "Waldemar",
-        img: castThumbnail1
-    },
-    {
-        id: "1f62fda1-4bc5-4317-9765-9c4a7cacb585",
-        name: "Grzegorz Gzyl",
-        role: "Kubek",
-        img: castThumbnail2
-    },
-    {
-        id: "388baf81-fb32-4f9d-8105-079c34d2c009",
-        name: "Bogdan Kalus",
-        role: "Słomiński",
-        img: castThumbnail3
-    },
-    {
-        id: "c43fa865-9cfa-4f70-a3f9-02b7c7a090ff",
-        name: "Dariusz Majchrzak",
-        role: "Melik",
-        img: castThumbnail4
-    }
-]
+export const cast = {
+    main: [
+        {
+            id: "04c8b670-3309-459c-a9a2-4be65f9a4d89",
+            name: "Piotr Głowacki",
+            role: "Waldemar",
+            img: castMainThumbnail1
+        },
+        {
+            id: "1f62fda1-4bc5-4317-9765-9c4a7cacb585",
+            name: "Grzegorz Gzyl",
+            role: "Kubek",
+            img: castMainThumbnail1
+        },
+        {
+            id: "388baf81-fb32-4f9d-8105-079c34d2c009",
+            name: "Bogdan Kalus",
+            role: "Słomiński",
+            img: castMainThumbnail1
+        },
+        {
+            id: "c43fa865-9cfa-4f70-a3f9-02b7c7a090ff",
+            name: "Dariusz Majchrzak",
+            role: "Melik",
+            img: castMainThumbnail1
+        },
+        {
+            id: "b1e50c1d-1fe6-4ea2-82ae-4a45b4b954ad",
+            name: "Agata Moszumańska",
+            role: "Kelnerka",
+            img: castMainThumbnail1
+        },
+        {
+            id: "f77b3052-0726-4cad-9731-8ee70ff53557",
+            name: "Ireneusz Pastuszak",
+            role: "Wtorek",
+            img: castMainThumbnail1
+        },
+        {
+            id: "47c230aa-0caf-43a6-be72-f293ca355b63",
+            name: "Tomasz Więcki",
+            role: "Sympatyk",
+            img: castMainThumbnail1
+        },
+        {
+            id: "25a13ad4-fc88-4c83-aacc-bdbcd1cef1b2",
+            name: "Piotr Witkowski",
+            role: "Ochroniarz",
+            img: castMainThumbnail1
+        }
+    ],
+    other: [
+        {
+            id: "af9851de-8865-45b1-b477-db50da235dcf",
+            name: "Gabriel Marciniak",
+            role: "Policjant",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "0349e94d-3a7b-4f30-a372-8645ed1514a4",
+            name: "Adrian Jasiński",
+            role: "Porucznik",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "898593d7-0e42-41e2-b0e3-5cdfec0619cf",
+            name: "Ignacy Wiśniewski",
+            role: "Analityk",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "e7643789-db63-43db-8147-db3324478d42",
+            name: "Gracjan Sokołowski",
+            role: "Brandon",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "af9851de-8865-45b1-b477-db50da235dcf",
+            name: "Jacek Sikora",
+            role: "Biegacz",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "0349e94d-3a7b-4f30-a372-8645ed1514a4",
+            name: "Alfred Rutkowski",
+            role: "Pracownik biura",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "898593d7-0e42-41e2-b0e3-5cdfec0619cf",
+            name: "Kuba Mazur",
+            role: "Klientka kawiarni",
+            img: castOtherThumbnail1
+        },
+        {
+            id: "e7643789-db63-43db-8147-db3324478d42",
+            name: "Kazimierz Stępień",
+            role: "Klient baru",
+            img: castOtherThumbnail1
+        }
+    ],
+}
---
+++ app/src/data/creator.js
@@ -0,0 +1,28 @@
+import creatorThumbnail1 from "../assets/creator/creatorThumbnail1.jpg"
+
+export const creator = [
+    {
+        id: "04c8b670-3309-459c-a9a2-4be65f9a4d89",
+        name: "Piotr Głowacki",
+        role: "Waldemar",
+        img: creatorThumbnail1
+    },
+    {
+        id: "1f62fda1-4bc5-4317-9765-9c4a7cacb585",
+        name: "Grzegorz Gzyl",
+        role: "Kubek",
+        img: creatorThumbnail1
+    },
+    {
+        id: "388baf81-fb32-4f9d-8105-079c34d2c009",
+        name: "Bogdan Kalus",
+        role: "Słomiński",
+        img: creatorThumbnail1
+    },
+    {
+        id: "c43fa865-9cfa-4f70-a3f9-02b7c7a090ff",
+        name: "Dariusz Majchrzak",
+        role: "Melik",
+        img: creatorThumbnail1
+    }
+]
