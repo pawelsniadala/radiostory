713a337592e3ca12cb69840938c1c027a32c8d9f
Paweł Śniadała <biner.home@gmail.com>
2022-06-16 21:06:42 +0200

    Updates

--- app/src/components/HeaderPage.js
+++
@@ -1,9 +0,0 @@
-const HeaderPage = ({ header }) => (
-    <div className="header page">
-        <h2 className="text">
-            {header}
-        </h2>
-    </div>
-);
-
-export default HeaderPage;
