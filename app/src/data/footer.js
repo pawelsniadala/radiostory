c5da947f230123592580b053c29ea124176ed4d3
Paweł Śniadała <biner.home@gmail.com>
2022-06-18 05:36:35 +0200

    Adding footer data

---
+++ app/src/data/footer.js
@@ -0,0 +1,105 @@
+export const footer = {
+    business: [
+        {
+            id: "3f83e609-c625-4a9d-81e1-350c67307d62",
+            name: "Brodacz Media Mariusz Śniadała",
+            address: "Kornaciska 19, 07-210 Długosiodło",
+            nip: "NIP: 7622014234",
+            regon: "REGON: 521119841"
+        }
+    ],
+    video: [
+        {
+            id: "501a147a-8c10-41d3-a480-fbded1ed5e58",
+            name: "W rolach głównych",
+            href: "/cast/main"
+        },
+        {
+            id: "9061c854-4485-48ec-a919-656d4f9213a3",
+            name: "W pozostałych rolach",
+            href: "/cast/other"
+        }
+    ],
+    photo: [
+        {
+            id: "7df3d831-077c-4b02-9a70-61156e8bf0aa",
+            name: "Scenariusz i reżyseria",
+            href: "/creator/direction"
+        },
+        {
+            id: "a0ebe250-6c6b-4bc6-afd3-a9cfe289cdfc",
+            name: "Zdjęcia",
+            href: "/creator/photo"
+        },
+        {
+            id: "c13886b7-7667-4697-844d-006061724e46",
+            name: "Montaż",
+            href: "/creator/montage"
+        },
+        {
+            id: "1856c68f-e3df-478f-b8b3-81025488246b",
+            name: "Charakteryzacja",
+            href: "/creator/makeup"
+        },
+        {
+            id: "2bae6379-91c1-466b-a96f-4c17bec71c77",
+            name: "Kierownictwo planu",
+            href: "/creator/management"
+        },
+        {
+            id: "ae372670-5a3a-4b96-9a2a-65dd03554d6e",
+            name: "Operatorzy kamer",
+            href: "/creator/camera"
+        }
+    ],
+    contact: {
+        details: [
+            {
+                id: "e28fbb3e-5c0e-456a-a5cc-6d76402eb950",
+                label: "phone",
+                title: "Zadzwoń",
+                href: "tel:+48501122980",
+                content: "+48 501 122 980"
+            },
+            {
+                id: "67028c30-d548-4ada-98c1-49fc3374c7f5",
+                label: "email",
+                title: "Napisz wiadomość",
+                href: "mailto:biuro@radiostory.pl",
+                content: "biuro@radiostory.pl"
+            }
+        ],
+        media: [
+            {
+                id: "a90ee660-0bf4-40f5-8bf6-738de9146093",
+                label: "facebook",
+                title: "Facebook",
+                href: "https://www.facebook.com/brodaczmedia1/",
+            },
+            {
+                id: "78c29e31-02ad-4efa-aff4-bbed908b4ea7",
+                label: "instagram",
+                title: "Instagram",
+                href: "https://www.instagram.com/brodaczmedia/",
+            },
+            {
+                id: "5391b681-0ed2-4846-9901-13ef451f6350",
+                label: "youtube",
+                title: "YouTube",
+                href: "https://www.youtube.com/channel/UCNbuL274tzbrmcqFOtcGYaw",
+            },
+            {
+                id: "94b08002-d906-4817-a153-1191538ac727",
+                label: "tiktok",
+                title: "TikTok",
+                href: "https://www.tiktok.com/@brodaczmedia",
+            },
+            {
+                id: "2e852bd2-3dd4-4bc2-ac31-85e5eccca92c",
+                label: "vimeo",
+                title: "Vimeo",
+                href: "https://vimeo.com/brodaczmedia",
+            },
+        ]
+    }
+};
