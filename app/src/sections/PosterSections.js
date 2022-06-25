d5f48376c991e2326c8db24204063ee2dabb447c
Paweł Śniadała <biner.home@gmail.com>
2022-06-25 14:16:50 +0200

    Adding information to the Poster section

--- app/src/sections/PosterSections.js
+++ app/src/sections/PosterSections.js
@@ -1,6 +1,3 @@
-// import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-
 const PosterSection = () => {
     return (
         <div className="jumbotron">
@@ -8,13 +5,47 @@ const PosterSection = () => {
                 <div className="content-wrapper">
                     <div className="company-wrapper">
                         <h1 className="display-1 company-name">Radiostory</h1>
-                        {/* <span className="company-services">
-                            film
-                            <span className="dot">&#xb7;</span>
-                            fotografia
-                            <span className="dot">&#xb7;</span>
-                            dźwięk
-                        </span> */}
+                        <div className="description container">
+                            <span className="text">
+                                <span className="item heading">Scenariusz i reżyseria:</span>
+                                <span className="item">Maciej Hydr</span>
+                            </span>
+                            <span className="text">
+                                <span className="item heading">Obsada:</span>
+                                <span className="item">Piotr Głowacki</span>
+                                <span className="item">Grzegorz Gzyl</span>
+                                <span className="item">Bogdan Kalus</span>
+                                <span className="item">Dariusz Majchrzak</span>
+                                <span className="item">Agata Moszumańska</span>
+                                <span className="item">Irena Pastuszak</span>
+                                <span className="item">Tomasz Więcki</span>
+                                <span className="item">Piotr Witkowski</span>
+                            </span>
+                            <span className="text">
+                                <span className="item heading">Zdjęcia:</span>
+                                <span className="item">Aleksander Krzystyniak</span>
+                                <span className="item">Michał Środek</span>
+                            </span>
+                            <span className="text">
+                                <span className="item heading">Montaż:</span>
+                                <span className="item">Jakub Kopeć PSM</span>
+                            </span>
+                            <span className="text">
+                                <span className="item heading">Charakteryzacja:</span>
+                                <span className="item">Dominika Hamziuk</span>
+                                <span className="item">Katarzyna Gulbierz</span>
+                            </span>
+                            <span className="text">
+                                <span className="item heading">Kierownictwo planu:</span>
+                                <span className="item">Maciej Hydr</span>
+                                <span className="item">Tomasz Kazimierczak</span>
+                                <span className="item">Mateusz Rzońca</span>
+                                <span className="item">Natalia Kuna</span>
+                                <span className="item">Aleksandra Kardynał</span>
+                            </span>
+                            <hr/>
+                            <span className="info">W kiach od 4 września</span>
+                        </div>
                     </div>
                 </div>
             </div>
