1e9ccaef19eefcdb35117fa7c57153f6af9feaec
Paweł Śniadała <biner.home@gmail.com>
2022-07-02 04:05:03 +0200

    Repair icons svg

--- app/src/assets/navbar/YouTubeSvg.js
+++ app/src/assets/navbar/YouTubeSvg.js
@@ -1,11 +1,11 @@
 const YouTubeSvg = () => (
     <svg xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 3333 3333"
-        shape-rendering="geometricPrecision"
-        text-rendering="geometricPrecision"
-        image-rendering="optimizeQuality"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+        imageRendering="optimizeQuality"
+        fillRule="evenodd"
+        clipRule="evenodd"
         width="28px"
         height="28px"
     >
