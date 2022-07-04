94ffc4305bf662741ed092b41f13726533292ca3
Paweł Śniadała <biner.home@gmail.com>
2022-07-04 05:32:36 +0200

    Modification of the Card component

--- app/src/components/CardPhoto.js
+++ app/src/components/CardPhoto.js
@@ -1,14 +1,16 @@
 import ReactImageAppear from "react-image-appear";
-import ZoomInIcon from "@mui/icons-material/ZoomIn";
+import SearchZoomSvg from "../assets/photo/SearchZoomSvg";
 
 const CardPhoto = ({
     cardTitle,
     cardPhotoSrc,
     layoutRef,
-    onClick,
+    onClick
 }) => (
     <div className="card photo">
-        <div className="img-box" ref={layoutRef}>
+        <div className="img-box"
+            ref={layoutRef}
+        >
             <ReactImageAppear
                 src={cardPhotoSrc}
                 onClick={onClick}
@@ -17,34 +19,21 @@ const CardPhoto = ({
                 placeholderStyle={{ backgroundColor: "transparent", display: "inline" }}
                 showLoader={false}
             />
-            <span onClick={onClick}>
-                <ZoomInIcon sx={{ fontSize: 60 }} />
+            <span className="icon-wrapper" onClick={onClick}>
+                <SearchZoomSvg />
             </span>
-            {/* <div class="MuiImageListItemBar-root MuiImageListItemBar-positionBottom css-ztrqiq-MuiImageListItemBar-root">
-                <div class="MuiImageListItemBar-titleWrap MuiImageListItemBar-titleWrapBottom MuiImageListItemBar-titleWrapActionPosRight css-14q7c0c-MuiImageListItemBar-titleWrap">
-                    <div class="MuiImageListItemBar-title css-dasnyc-MuiImageListItemBar-title">
-                        Breakfast
-                    </div>
-                    <div class="MuiImageListItemBar-subtitle css-hk6gwf-MuiImageListItemBar-subtitle">
-                        @bkristastucchio
-                    </div>
-                </div>
-            </div> */}
-            {cardTitle ? (
+            {cardTitle && (
                 <div className="description-wrapper">
                 <div className="text-wrapper">
                     <div className="title">
                         {cardTitle}
                     </div>
-                    {/* <div className="subtitle">
-                        @bkristastucchio
-                    </div> */}
+                    <div className="subtitle">
+                        Gdańsk
+                    </div>
                 </div>
             </div>
-            ) : (
-                null
             )}
-
         </div>
     </div>
 );
